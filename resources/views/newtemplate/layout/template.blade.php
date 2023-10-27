8fb0dcb76399cede3c4585a0c6b7409e56e67954
shafqat ghafoor <shafqatghafoor99@gmail.com>
2023-10-27 21:02:48 +0500

    kithcens

--- resources/views/newtemplate/layout/template.blade.php
+++ resources/views/newtemplate/layout/template.blade.php
@@ -888,10 +888,22 @@
 
 @yield('script')
 
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/653bc741a84dd54dc485ebee/1hdoo4meo';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
 
-
-<script src="https://static.elfsight.com/platform/platform.js" data-use-service-core defer></script>
-<div class="elfsight-app-4a5d05d9-91ce-41c1-953c-bb41b900b70f" data-elfsight-app-lazy></div>
+{{-- <script src="https://static.elfsight.com/platform/platform.js" data-use-service-core defer></script>
+<div class="elfsight-app-4a5d05d9-91ce-41c1-953c-bb41b900b70f" data-elfsight-app-lazy></div> --}}
 </body>
 
 
