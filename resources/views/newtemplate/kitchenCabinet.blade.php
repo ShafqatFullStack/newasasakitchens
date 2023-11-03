437a42aceffa09926b7425c85c3e7ae0c1b66f0e
shafqat ghafoor <shafqatghafoor99@gmail.com>
2023-11-03 21:15:03 +0500

    content

--- resources/views/newtemplate/kitchenCabinet.blade.php
+++ resources/views/newtemplate/kitchenCabinet.blade.php
@@ -389,15 +389,18 @@
         <div class="container" style="margin-top:20px;margin-bottom:20px;text-align:center">
             <div class="row">
                 <div class="col d-flex flex-row justify-content-between">
-                    <div class="col-md-4">
-                        <img src="{{ asset('bestAwards.png') }}" >
+                    <div class="col-md-3">
+                        <img src="{{ asset('recommend.png') }}" width=200 height=200>
                     </div>
 
-                    <div class="col-md-4">
-                        <img src="{{ asset('homestars.png') }}">
+                    <div class="col-md-3">
+                        <img src="{{ asset('homestars.png') }}" width=200 height=200>
                     </div>
-                    <div class="col-md-4">
-                        <img src="{{ asset('googleVerfied.png') }}">
+                    <div class="col-md-3">
+                        <img src="{{ asset('googleVerfied.png') }}" width=200 height=200>
+                    </div>
+                    <div class="col-md-3">
+                        <img src="{{ asset('houzz.png') }}" width=200 height=200>
                     </div>
 
                 </div>
